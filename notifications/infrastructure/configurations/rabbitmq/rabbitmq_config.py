6abf399512df6fbc14863df44c9fd4143346c9d1
Alan George Trinidad Cruz <100612234+AlanCruz10@users.noreply.github.com>
2024-06-28 00:10:15 -0600

    update: update ssl in rabbit configuration (#5)
    Co-authored-by: LarissaHilario <larihid1@gmail.com>

--- notifications/infrastructure/configurations/rabbitmq/rabbitmq_config.py
+++ notifications/infrastructure/configurations/rabbitmq/rabbitmq_config.py
@@ -1,6 +1,13 @@
 import pika
 import os
 from dotenv import load_dotenv
+import ssl
+
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+ssl_options=pika.SSLOptions(ssl_context)
 
 load_dotenv()
 hostname = os.getenv('RABBITMQ_HOST', 'localhost')
@@ -12,9 +19,10 @@ port = os.getenv('RABBITMQ_PORT')
 
 def setup_rabbitmq(queue_name, exchange_name, routing_key):
     credentials = pika.PlainCredentials(user, password)
-    parameters = pika.ConnectionParameters(hostname, port, '/', credentials)
+    parameters = pika.ConnectionParameters(hostname, port, '/', credentials, ssl_options)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
     channel.queue_declare(queue=queue_name, durable=True)
     channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=routing_key)
     return channel
+
