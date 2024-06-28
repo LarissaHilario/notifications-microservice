b731f87dca5b734f4ee6853777fd3c14255f7e87
LarissaHilario <larihid1@gmail.com>
2024-06-28 16:29:51 -0600

    fixed: fixed email subscription

--- notifications/infrastructure/controllers/controller_notification.py
+++ notifications/infrastructure/controllers/controller_notification.py
@@ -1,8 +1,52 @@
 from fastapi import APIRouter, HTTPException
+from os import getenv
+from dotenv import load_dotenv
+
+from notifications.application.useCases.send_notification_use_case import SendNotificationUseCase
+from notifications.infrastructure.repositories.notification_repository import NotificationRepository
+from notifications.infrastructure.utilities.formatters import formatter_message
+
+load_dotenv()
 
 routes = APIRouter()
+
+notification_repository = NotificationRepository("DB")
+
+notification_use_case = SendNotificationUseCase(notification_repository)
 
 
 @routes.get("/health")
 async def root():
     return {"message": "OK"}
+
+
+@routes.get("/send/email")
+async def root():
+    email = "kristellperez34@gmail.com"
+    email_test = "Test to send email to {customer_name} from {support_email} with number {support_phone}."
+    message_formatted = formatter_message(
+        email_test, "User Test",
+        getenv("SNS_EMAIL_SUPPORT"),
+        getenv("SNS_PHONE_NUMBER_SUPPORT")
+    )
+    subject = "Test to send email to user"
+    try:
+        notification_use_case.execute(email, message_formatted, subject)
+        base_response = {
+            "data": None,
+            "message": "Email send successfully",
+            "status": True,
+            "status_code": 200,
+            "http_status": "OK"
+        }
+    except Exception as e:
+        base_response = {
+            "data": None,
+            "message": str(e),
+            "status": False,
+            "status_code": 500,
+            "http_status": "Internal Server Error"
+        }
+
+    return base_response
+
--- notifications/infrastructure/repositories/notification_repository.py
+++ notifications/infrastructure/repositories/notification_repository.py
@@ -8,23 +8,37 @@ load_dotenv()
 
 
 class NotificationRepository(NotificationsInterface):
-    def __init__(self, db_connection):
+    def _init_(self, db_connection):
         self.db_connection = db_connection
 
     def send_email(self, email: str, message: str, subject: str) -> None:
+
         topic_arn = getenv("SNS_TOPIC_ARN")
-        response = sns_client.subscribe(
-            TopicArn=topic_arn,
-            Protocol="email",
-            Endpoint=email,
-            ReturnSubscriptionArn=True
-        )
-        time.sleep(50)
+
+        is_subscribed = False
+
+        paginator = sns_client.get_paginator('list_subscriptions_by_topic')
+        for page in paginator.paginate(TopicArn=topic_arn):
+            for subscription in page['Subscriptions']:
+                if subscription['Endpoint'] == email:
+                    is_subscribed = True
+
+        if not is_subscribed:
+            response = sns_client.subscribe(
+                TopicArn=topic_arn,
+                Protocol="email",
+                Endpoint=email,
+                ReturnSubscriptionArn=True
+            )
+            time.sleep(50)
+
         response = sns_client.publish(
             TopicArn=topic_arn,
             Message=message,
             Subject=subject
         )
+
         # sns_client.unsubscribe(
         #     SubscriptionArn=subscription_arn
         # )
+
