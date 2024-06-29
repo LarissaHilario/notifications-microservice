d626bb92feacdca668915609bddd9f40b7f5aef3
Crescens-Henry <crescencio.ps.code@gmail.com>
2024-06-28 22:45:26 -0600

    update: ensure correct db_connection instantiation and usage

--- notifications/application/useCases/send_notification_use_case.py
+++ notifications/application/useCases/send_notification_use_case.py
@@ -1,7 +1,53 @@
+from notifications.infrastructure.configurations.sns.sns_client_config import sns_client
+from dotenv import load_dotenv
+from os import getenv
+import time
+from notifications.domain.entities.notification import Notifications
+import datetime
+import uuid
+
+load_dotenv()
+
 
 class SendNotificationUseCase:
     def __init__(self, notification_repository):
         self.notification_repository = notification_repository
 
-    def execute(self, email, message, subject):
-        self.notification_repository.send_email(email, message, subject)
+    def execute(self, uuid_user, email, message, subject, type_notification):
+        topic_arn = getenv("SNS_TOPIC_ARN")
+
+        is_subscribed = False
+        paginator = sns_client.get_paginator('list_subscriptions_by_topic')
+        for page in paginator.paginate(TopicArn=topic_arn):
+            for subscription in page['Subscriptions']:
+                if subscription['Endpoint'] == email:
+                    is_subscribed = True
+                    break
+
+        if not is_subscribed:
+            response = sns_client.subscribe(
+                TopicArn=topic_arn,
+                Protocol="email",
+                Endpoint=email,
+                ReturnSubscriptionArn=True
+            )
+            time.sleep(50)
+
+        response = sns_client.publish(
+            TopicArn=topic_arn,
+            Message=message,
+            Subject=subject
+        )
+
+        notification_uuid = str(uuid.uuid4())
+        date_sent = datetime.datetime.now()
+        notification = Notifications(uuid=notification_uuid, type=type_notification, title=subject,
+                                     message=message, date_sent=date_sent)
+
+        try:
+            self.notification_repository.save_data(notification, uuid_user)
+        except Exception as e:
+            print(f"Error al guardar en la base de datos: {e}")
+            return False
+
+        return True
--- notifications/infrastructure/controllers/controller_notification.py
+++ notifications/infrastructure/controllers/controller_notification.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi import APIRouter, HTTPException
 from os import getenv
 from dotenv import load_dotenv
@@ -5,12 +6,15 @@ from dotenv import load_dotenv
 from notifications.application.useCases.send_notification_use_case import SendNotificationUseCase
 from notifications.infrastructure.repositories.notification_repository import NotificationRepository
 from notifications.infrastructure.utilities.formatters import formatter_message
+import uuid
+from notifications.infrastructure.enums.enum_type import Type
+from database.database import DBConnection
 
 load_dotenv()
 
 routes = APIRouter()
 
-notification_repository = NotificationRepository("DB")
+notification_repository = NotificationRepository(DBConnection())
 
 notification_use_case = SendNotificationUseCase(notification_repository)
 
@@ -22,6 +26,7 @@ async def root():
 
 @routes.get("/send/email")
 async def root():
+    uuid_user = str(uuid.uuid4())
     email = "kristellperez34@gmail.com"
     email_test = "Test to send email to {customer_name} from {support_email} with number {support_phone}."
     message_formatted = formatter_message(
@@ -30,15 +35,22 @@ async def root():
         getenv("SNS_PHONE_NUMBER_SUPPORT")
     )
     subject = "Test to send email to user"
+    type_notification = Type.TYPE_WELCOME.value
     try:
-        notification_use_case.execute(email, message_formatted, subject)
-        base_response = {
-            "data": None,
-            "message": "Email send successfully",
-            "status": True,
-            "status_code": 200,
-            "http_status": "OK"
-        }
+        status, response = notification_use_case.execute(
+            uuid_user, email, message_formatted, subject, type_notification)
+
+        if not status:
+            raise HTTPException(
+                status_code=500, detail="Internal Server Error")
+        else:
+            base_response = {
+                "data": response,
+                "message": "Email send successfully",
+                "status": True,
+                "status_code": 200,
+                "http_status": "OK"
+            }
     except Exception as e:
         base_response = {
             "data": None,
